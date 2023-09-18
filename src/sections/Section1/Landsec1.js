7584ca6fc0adc3c79bfc95f48921181228d30672
jeremyasirwaad <asirwaadj@gmail.com>
2023-09-18 22:59:36 +0530

    done

--- src/sections/Section1/Landsec1.js
+++ src/sections/Section1/Landsec1.js
@@ -3,6 +3,7 @@ import "./Landsec1.css";
 import logo from "../../Asserts/logo.png";
 import AOS from "aos";
 import "aos/dist/aos.css";
+import signupbtn from "../../Asserts/signup.png";
 
 export const Landsec1 = () => {
 	useEffect(() => {
@@ -15,6 +16,21 @@ export const Landsec1 = () => {
 			<span className="landsec1header" data-aos="fade-right">
 				Beta Testing <br /> is <span style={{ color: "#FF9595" }}>LIVE!</span>{" "}
 			</span>
+			<div className="sec3textland">
+				<a
+					data-aos="flip-left"
+					style={{
+						width: "100%",
+						display: "flex",
+						alignItems: "center",
+						justifyContent: "center"
+					}}
+					href="https://forms.gle/Y4RMaVwtw6MNct8B8"
+					target="_blank"
+				>
+					<img className="reportbtn" src={signupbtn} alt="" />
+				</a>
+			</div>
 			<span data-aos="fade-left" className="landsec1subtitile">
 				<span style={{ fontWeight: "700" }}>$3500 </span>
 				worth of <br /> USDT up for Grabs!!!
