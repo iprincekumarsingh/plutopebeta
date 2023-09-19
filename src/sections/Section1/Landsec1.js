bd89825411f5e7fff3e964b446d8e359a52977ed
Jeremy Asirwaad P <71336703+jeremyasirwaad@users.noreply.github.com>
2023-09-19 13:40:47 +0530

    Update Landsec1.js

--- src/sections/Section1/Landsec1.js
+++ src/sections/Section1/Landsec1.js
@@ -16,21 +16,21 @@ export const Landsec1 = () => {
 			<span className="landsec1header" data-aos="fade-right">
 				Beta Testing <br /> is <span style={{ color: "#FF9595" }}>LIVE!</span>{" "}
 			</span>
-			<div className="sec3textland">
-				<a
-					data-aos="flip-left"
-					style={{
-						width: "100%",
-						display: "flex",
-						alignItems: "center",
-						justifyContent: "center"
-					}}
-					href="https://forms.gle/Y4RMaVwtw6MNct8B8"
-					target="_blank"
-				>
-					<img className="reportbtn" src={signupbtn} alt="" />
-				</a>
-			</div>
+			// <div className="sec3textland">
+			// 	<a
+			// 		data-aos="flip-left"
+			// 		style={{
+			// 			width: "100%",
+			// 			display: "flex",
+			// 			alignItems: "center",
+			// 			justifyContent: "center"
+			// 		}}
+			// 		href="https://forms.gle/Y4RMaVwtw6MNct8B8"
+			// 		target="_blank"
+			// 	>
+			// 		<img className="reportbtn" src={signupbtn} alt="" />
+			// 	</a>
+			// </div>
 			<span data-aos="fade-left" className="landsec1subtitile">
 				<span style={{ fontWeight: "700" }}>$3500 </span>
 				worth of <br /> USDT up for Grabs!!!
