c49158c76e7a751675f213a82b0f56c9e6c2e589
jeremyasirwaad <asirwaadj@gmail.com>
2023-09-20 00:52:48 +0530

    done

--- src/sections/Footer/Footer.js
+++ src/sections/Footer/Footer.js
@@ -42,8 +42,24 @@ export const Footer = () => {
 			<div className="footermobile">
 				<div className="footersec">
 					<span className="footh">Legal</span>
-					<span>Privacy Notice</span>
-					<span>Terms of use</span>
+					<span
+						onClick={(e) => {
+							e.preventDefault();
+							window.location =
+								"https://docs.google.com/document/d/1SyAvTXwXT9eAnOUfgekwYtn6kejd6NoL1y8g8qImjvU/edit?usp=drivesdk";
+						}}
+					>
+						Privacy Notice
+					</span>
+					<span
+						onClick={(e) => {
+							e.preventDefault();
+							window.location =
+								"https://docs.google.com/document/d/1Ctlb01w7d_qnrnr_tdmK4gZm3Ur3GZ5euOBx11dsZi0/edit?usp=drivesdk	";
+						}}
+					>
+						Terms of use
+					</span>
 					<div className="footicons">
 						<LiaDiscord
 							onClick={() => {
